f4f8e1fc7e302b401caf46836992e92168c51922
Kevin <kevinolongo9@gmail.com>
2023-09-24 22:15:53 +0300

    Modified Netpay.js file

--- Netpay.js
+++ Netpay.js
@@ -1,5 +1,5 @@
-function payee(grossSalary) {
-    let netIncome = 0
+function payee(grossSalary,benefits ) {
+    let netIncome = grossSalary + benefits
     
       
     
@@ -8,23 +8,22 @@ function payee(grossSalary) {
         
       }
       else if (grossSalary >= 24001 && grossSalary <= 32333){
-        netIncome = grossSalary - (300+((grossSalary*0.15)*2))
+        netIncome = grossSalary + benefits - (300+((grossSalary*0.15)*2))
         
       }
       else if (grossSalary >= 32334 && grossSalary <= 40667){
-        netIncome = grossSalary -(400+((grossSalary*0.2)*2))
+        netIncome = grossSalary + benefits -(400+((grossSalary*0.2)*2))
         
       }
       else if (grossSalary >= 40668 && grossSalary <= 49000){
-        netIncome = grossSalary -(500+((grossSalary*0.25)*2))
+        netIncome = grossSalary + benefits-(500+((grossSalary*0.25)*2))
         
       }
       else { 
-        netIncome = grossSalary -(600+((grossSalary*0.3)*2))
+        netIncome = grossSalary + benefits -(600+((grossSalary*0.3)*2))
         
          }
       console.log(netIncome)
     }
     
-    payee(30000) 
-    
+    payee(30000, 10000) 
